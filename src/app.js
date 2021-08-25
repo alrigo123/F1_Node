d8bc3ed4402a4660f533b3a7a17f322b237c8eb0
alrigo123 <alexestrada2804@gmail.com>
2021-08-24 22:30:09 -0500

    React added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const mysql = require('mysql');
 const myConnection = require('express-myconnection');
 const colors = require('colors');
 const app= express();
+const MongoClient = require('mongodb').MongoClient;
+
 
 //settings
 app.set('port',process.env.PORT || 2500);
@@ -13,8 +15,18 @@ app.set('port',process.env.PORT || 2500);
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
+//connection string
+const uri = 'mongodb://localhost/formula';
+const con = new MongoClient(uri,{useNewParsers:true});
+    //if connection true
+    con.connect(err => {
+        //define table
+        const pilots = con.db("formula").collection('pilot');
 
-
+        //Insert data pilot 
+        let pilot = {id = 1 , name = "Lewis" , last_name = "Hamilton", country: "United Kingdom", 
+        age : "30", points:"130", photo:"lewis.jpg", id_team : "1"}
+    })
 
 
 module.exports = app;
