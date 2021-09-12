e67ac64a5195c9f5da2d6d193c68f5492ae9b717
alrigo123 <alexestrada2804@gmail.com>
2021-09-11 22:23:01 -0500

    Interfaces donde and little advancded of controller

--- src/controllers/pilotcontroller.js
+++ src/controllers/pilotcontroller.js
@@ -1,21 +1,38 @@
-let conex = require('../config/conexion');
+let conex = require('../config/conexion')
 
 const controller = {}
 
 //to show the pilots in the query
-controller.list = (req,res)=>{
-        conex.query('select * from pilot',(err,pilots)=>{
-            if(err){
-                res.json(err)
-            } 
-            res.render('position',{
-                title : 'Position from this website',
-                data : pilots
-            });
+controller.list = (req, res) => {
+  conex.query('select * from pilot', (err, pilots) => {
+    if (err) {
+      res.json(err)
+    }
+    res.render('position', {
+      title: 'Position season 2021',
+      data: pilots,
+    })
+  })
+}
+controller.listControl = (req, res) => {
+  ;[
+    conex.query(
+      'SELECT P.id_pilot,P.name_pilot,P.last_name,P.nick,P.country_pilot,P.points, T.id_team,T.name_team FROM pilot P JOIN team T ON P.id_team = T.id_team',
+      (err, pilots) => {
+        if (err) {
+          res.json(err)
+        }
+        res.render('./templates/listPilot', {
+          title: 'Pilots from 2021 season',
+          data: pilots,
         })
-   
+      },
+    ),
+  ]
 }
 
-
+/*
+SELECT P.id_pilot, T.id_team,T.name,P.name FROM pilot P JOIN team T ON P.id_team = T.id_team;
+*/
 
 module.exports = controller
--- src/routes/index.js
+++ src/routes/index.js
@@ -14,14 +14,17 @@ router.get('/pilot',(req,res)=>{
     res.render('pilot', {title : 'Pilot from this website'})
 })
 
-router.get('/listPilot',(req,res)=>{
-    res.render('./templates/listPilot', {title : 'Pilots to edit'});
-});
+router.get('/position',controller.list)
+
+router.get('/listPilot', controller.listControl);
 
 router.get('/newPilot', (req, res) => {
 res.render('./templates/newPilot',{title : 'New Pilots to add'})
 });
 
-router.get('/position',controller.list)
+router.get('/editPilot', (req, res) => {
+res.render('./templates/editPilot',{title : 'Pilot to Edit'})
+});
+
 
 module.exports = router
