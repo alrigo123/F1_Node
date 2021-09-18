b4156803d5fbda7a9e4d5d23eee22d54229e9713
alrigo123 <alexestrada2804@gmail.com>
2021-09-17 22:21:16 -0500

    Save Pilot with multer

--- src/app.js
+++ src/app.js
@@ -1,48 +1,66 @@
 //
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const mysql = require('mysql');
-const myConnection = require('express-myconnection');
-const colors = require('colors');
-const app= express();
-const MongoClient = require('mongodb').MongoClient;
-const index = require('./routes/index') 
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const mysql = require('mysql')
+const myConnection = require('express-myconnection')
+const colors = require('colors')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const index = require('./routes/index')
 const polka = require('polka')
 const server = polka()
+const multer = require('multer')
 //settings
-app.set('port',process.env.PORT || 2828);
-//template which we are gonna work
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'views'))
-//static
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('port', process.env.PORT || 2828)
 
 
 //middlewares  falta multer y otrs maasxdxdxd
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
-app.use(express.urlencoded({extended : false}));
-app.use(express.json());
-//midelwares , we can use multer ,and we have documentation about how to use 
-    
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+//midelwares , we can use multer ,and we have documentation about how to use
+/*multer*/
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, './public/img/uploads'),
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+app.use(
+  multer({
+    storage : storage,
+    dest: path.join(__dirname, './public/img/uploads'),
+    limits: { filesize: 10000 },
+    fileFilter: (req, file, cb, err) => {
+      const filetype = /jpeg|jpg|png|gif/
+      const mimetype = filetype.test(file.mimetype)
+      const extname = filetype.test(path.extname(file.originalname))
+      if (mimetype && extname) {
+        return cb(null, true)
+      } else {
+        cb('Error: File must be a JPG, PNG, or GIF', err)
+      }
+    },
+  }).single('photo_pilot'),
+)
+
+
+
+//template which we are gonna work
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+//static
+app.use(express.static(path.join(__dirname, 'public')))
+
 //routes
-app.use('/', index);
-
-
+app.use('/', index)
 
 //listen the server
 app.listen(app.get('port'), (req, res) => {
   console.log(`Listening on port ${app.get('port')}!!`)
-});
+})
 
-module.exports = app;
-
-
-
-
-
-
-
-
-
+module.exports = app
--- src/controllers/datacontroller.js
+++ src/controllers/datacontroller.js
@@ -4,11 +4,11 @@ let conex = require('../config/conexion')
 const controller = {}
 
 controller.delete = (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   console.log(req.params)
-  conex.query('DELETE FROM pilot where id_pilot = ?',[id],(err,pilot)=>{
-      res.redirect('/listPilot');
-      console.log(pilot)
+  conex.query('DELETE FROM pilot where id_pilot = ?', [id], (err, pilot) => {
+    res.redirect('/listPilot')
+    console.log(pilot)
   })
 }
 
@@ -17,35 +17,44 @@ controller.edit = (req, res) => {
   const aux = req.params.id_pilot
   // console.log({ id_pilot }, req.params)
   //console.log(aux)
-  conex.query('select * from pilot where id_pilot = ?', [id], async(err, pilot) => {
-    if (err) {
-      throw err
-    } else {
-      res.render('./templates/editPilot', {
-        //porque cusotmer es arreglo
-        title: 'hola',
-        data: pilot[0],
+  conex.query(
+    'select * from pilot where id_pilot = ?',
+    [id],
+    async (err, pilot) => {
+      if (err) throw err
+      conex.query('SELECT id_team,name_team FROM team', async (err, teams) => {
+        if (err) throw err
+
+        res.render('./templates/editPilot', {
+          //porque cusotmer es arreglo
+          title: 'hola',
+          data: pilot[0],
+          equipos: teams,
+        })
       })
-    }
-  })
-  /*
-   conex.query('SELECT id_team,name_team FROM team', async (err, teams) => {
-    if (err) {
-      return res.json(err)
-    } else {
-     await res.render('./templates/editPilot', {
-        equipos: teams
-      })
-    }
-  })
-  */
+    },
+  )
 }
 
 //when i submit the form
-controller.update = (req, res) => {}
+controller.update = (req, res) => {
+  const { id } = req.params
+  const newPilot = req.body
+  conex.query(
+    'UPDATE pilot set ? where id_pilot = ?',
+    [newPilot, id],
+    (err, rows) => {
+      res.redirect('/listPilot')
+    },
+  )
+}
 
 //insert new pilot
 controller.add = (req, res) => {
+  console.log('File: ', req.file)
+  //Undefined because miss enctype in ejs form
+  console.log(req.body)
+
   const data = req.body
   //console.log("esta es la dataaaaa",data)
   conex.query('INSERT INTO pilot set ?', [data], (err, pilot) => {
